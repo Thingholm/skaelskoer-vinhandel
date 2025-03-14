5534947adfd19e6bf58f7b07ac299f83318d761a
Gustav Nielsen <gustavnielsen1234@gmail.com>
2025-03-14 12:42:22 +0100

    Added logo and started on Index

--- skaelskoer-vinhandel-app/app/index.tsx
+++ skaelskoer-vinhandel-app/app/index.tsx
@@ -1,15 +1,27 @@
-import { Text, View } from "react-native";
+
+import { Text, View, StyleSheet, Image } from "react-native";
+
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <ParallaxScrollView
+      headerBackgroundColor={{light: "#FFFFFD", dark: "#003538"}}
+      headerImage={
+        <Image
+        source={require('@/assets/images/Skaelskoer_vinhandel_logo.png')}
+        />
+      }>
+    </ParallaxScrollView>
   );
 }
+
+const styles = StyleSheet.create ({
+formatting: {
+  flex: 1, 
+  justifyContent: "center",
+  alignItems: "center",
+},
+headerImage: {
+
+},
+});
